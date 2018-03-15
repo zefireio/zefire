b1f3434691afc4fc8d26790e25b1eb591f07189b
Zefire <zefireframework@gmail.com>
2018-03-15 21:56:03 +0000

    Basic views

--- resources/templates/layout/master.php
+++ resources/templates/layout/master.php
@@ -4,8 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="zefire.io">
+    <meta name="description" content="">
     <meta name="author" content="">
     <meta name="csrf-token" content="{{ \App::csrfToken() }}">
     <title>{{ \App::config('app.name') }} - {{ \App::config('app.description') }}</title>
@@ -13,21 +12,8 @@
     <link href="/assets/css/bootstrap.min.css" rel="stylesheet">
     <!-- Bootstrap theme -->
     <link href="/assets/css/bootstrap-theme.min.css" rel="stylesheet">
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <link href="/assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
     <!-- Custom styles for this template -->
-    <link href="/assets/css/theme.css" rel="stylesheet">
-    <link href="/assets/css/doc.css" rel="stylesheet">
-    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-    <!--[if lt IE 9]><script src="/assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-    <script src="/assets/js/ie-emulation-modes-warning.js"></script>
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/default.min.css"> 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/8.9.1/highlight.min.js"></script>
+    <link href="/assets/css/theme.css" rel="stylesheet">    
   </head>
   <body>
     <!-- Fixed navbar -->
@@ -40,15 +26,11 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="/"><img src="/assets/img/flame-logo.png" class="logo"></a>
           <a class="navbar-brand brand" href="/">{{ \App::config('app.name') }}</a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
           <ul class="nav navbar-nav navbar-right">
-            <li><a href="/documentation">Documentation</a></li>
-            <li><a href="/api-documentation" target="_blank">API Documentation</a></li>
-            <li><a href="/contribute">Contribute</a></li>
-            <li><a href="https://github.com/zefireio" target="_blank">Github</a></li>
+            <li><a href="/">Home</a></li>            
           </ul>
         </div>
       </div>
@@ -58,14 +40,10 @@
     </div>
     <footer class="footer">
       <div class="container">
-        Runtime: {{ \App::runtime() }}
+        
       </div>
     </footer>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="/assets/js/bootstrap.min.js"></script>
-    <script src="/assets/js/jquery.scrollTo.min.js"></script>
-    <script src="/assets/js/jquery.matchHeight-min.js"></script>
-    <script src="/assets/js/main.js"></script>    
-    <script src="/assets/js/ie10-viewport-bug-workaround.js"></script>
   </body>
 </html>
