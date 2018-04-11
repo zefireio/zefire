12e3b4b4e35043e41db71208122e57a03a77386b
Zefire <zefireframework@gmail.com>
2018-04-11 16:31:27 +0100

    Added default events

--- config/events.php
+++ config/events.php
@@ -10,9 +10,12 @@
     */
 
 return [
-    'app-message' => \Zefire\Core\Events\AppMessage::class,
-    'db-connect' => \Zefire\Database\Events\Connect::class,
-    'db-query' => \Zefire\Database\Events\Query::class,
-    'app-auth' => \Zefire\Authentication\Events\AuthEvent::class,
-    'app-logout' => \Zefire\Authentication\Events\LogoutEvent::class,
+    'app-message'       => \Zefire\Core\Events\AppMessage::class,
+    'db-connect'        => \Zefire\Database\Events\Connect::class,
+    'db-query'          => \Zefire\Database\Events\Query::class,
+    'app-auth'          => \Zefire\Authentication\Events\AuthEvent::class,
+    'app-logout'        => \Zefire\Authentication\Events\LogoutEvent::class,
+    'queue-push'        => \Zefire\queue\Events\PushToQueue::class,
+    'queue-process'     => \Zefire\queue\Events\ProcessJob::class,
+    'queue-job-status'  => \Zefire\queue\Events\JobStatus::class,
 ];
