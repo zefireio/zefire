da047e2491981683d00fe3dd1d7192ba02e328b2
Samuel Barthe <samb@wattbike.com>
2018-05-20 18:40:13 +0100

    Added user managment on basic install

---
+++ app/Controllers/Http/Auth/Authenticate.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Controllers\Http\Auth;
+
+use Zefire\Controllers\Auth\Authenticate as Auth;
+
+class Authenticate extends Auth
+{
+	//	
+}
---
+++ app/Controllers/Http/Auth/Register.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Controllers\Http\Auth;
+
+use Zefire\Controllers\Auth\Registration;
+
+class Register extends Registration
+{
+	//
+}
